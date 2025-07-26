ef3430dd67f8d90aca13004902e6eda7cad60004
ParasKanchan <paraskanchan438@gmail.com>
2025-07-26 17:26:00 +0545

    debugging

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,26 +1,24 @@
-import SearchBox from "./SearchBox"
-import InfoBox from "./InfoBox"
-import { useState } from "react"
+import SearchBox from "./SearchBox";
+import InfoBox from "./InfoBox";
+import { useState } from "react";
 
-export default function WeatherApp(){
-    let [weatherInfo, setWeatherInfo] = useState({
-       city : "Pilibhit",
-       feels_like : 299.57,
-       humidity :  76,
-       temp :  299.57,
-       temp_max :  299.57,
-       temp_min : 299.57,
-       weather : "haze"
-    });
+export default function WeatherApp() {
+  const [weatherInfo, setWeatherInfo] = useState(null); // No default city
 
-    let updateInfo = (newInfo) =>{
-        setWeatherInfo( newInfo);
-    }
+  const updateInfo = (newInfo) => {
+    setWeatherInfo(newInfo);
+  };
 
-    return(
-        <div>
-        <SearchBox updateInfo={updateInfo}/>
-        <InfoBox info = {weatherInfo}/>
-        </div>
-    )
+  return (
+    <div>
+      <SearchBox updateInfo={updateInfo} />
+      {weatherInfo ? (
+        <InfoBox info={weatherInfo} />
+      ) : (
+        <p style={{ textAlign: "center", marginTop: "20px" }}>
+          Please search for a city to view weather.
+        </p>
+      )}
+    </div>
+  );
 }
